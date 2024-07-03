80729756c754310bb519afd91580f1a4fac3344f
gerwalzer <german.walzer@davinci.edu.ar>
2024-07-03 10:14:05 -0300

    Login style and functionality (layout heigh and width, placeholder's color and height, when you use the keyboard, the logo and placeholder will fits to the screen) Configuration in AndroidManifest.xml
    TextViews on Strings.xml for Login, Password and Mail
    
    New files on create of FavFragmet class, its for favorites per user and layout archive for favorites. This function are created and used on MainActivity

--- app/src/main/java/com/example/lectorkeroyume/MainActivity.java
+++ app/src/main/java/com/example/lectorkeroyume/MainActivity.java
@@ -65,6 +65,9 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             case R.id.nav_home:
                 getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
                 break;
+            case R.id.nav_favorite:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
+                break;
             case R.id.nav_settings:
                 getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new SettingsFragment()).commit();
                 break;
